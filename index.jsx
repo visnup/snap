85f98c6526cd4b9a38ed41462a6969b4977f99ba
Visnu Pitiyanuvath <visnu@opendoor.com>
2015-10-06 21:32:44 -0700

    feels "ok" on an iphone

--- index.jsx
+++ index.jsx
@@ -23,53 +23,36 @@ angular
         if (isTouchSupported && !isScrollSnapSupported) {
           this.$element
             .addClass('snap')
-            .on('touchstart', this.onTouchStart.bind(this))
-            .on('touchend', this.onTouchEnd.bind(this))
-            .on('scroll', this.onScroll.bind(this));
+            .on('touchmove', this.onTouchMove.bind(this))
+            .on('touchend', this.onTouchEnd.bind(this));
         }
       }
 
-      onTouchStart(e) {
-        this.t0 = Date.now();
-      }
-
-      onTouchEnd(e) {
-        const x = this.$element.scrollLeft(),
-              xf = x + this.v * 100;
-        console.log({ x: x, v: this.v, xf: xf, width: this.width });
-        //this.xf = Math.round(x / this.width) * this.width;
-        //var dt = Math.min(Math.abs((this.xf - x) / (this.v0 || 200)), 300);
-
-        this.$element
-          .off('scroll')
-          .css('overflow', 'hidden');
-        $timeout(() => {
-          this.$element
-            .css('overflow', 'scroll')
-            .scrollLeft(xf, 100, 'easeOutQuad').finally(() => {
-              this.$element.on('scroll', this.onScroll.bind(this));
-            });
-        }, 0);
-
-        // I've decided I know where you're going. Let me help you along.
-        //console.log('xf:', this.xf);
-      }
-
-      onScroll(e) {
+      onTouchMove(e) {
         const x = this.$element.scrollLeft(),
               t = Date.now(),
               dt = t - this.t0;
 
-        if (dt < 5)
+        if (dt < 8)
           return;
 
         this.v = (x - this.x0) / dt;
-        //console.log({ x: x, dt: dt, v: this.v });
         this.x0 = x;
         this.t0 = t;
       }
 
-      haltUserScrolling(xf) {
+      onTouchEnd() {
+        const x = this.$element.scrollLeft(),
+              xf = Math.round((x + this.v * 100) / this.width) * this.width,
+              dt = Math.max((xf - x) / this.v, 200);
+
+        this.$element
+          .css('overflow', 'hidden')
+        $timeout(() => {
+          this.$element
+            .css('overflow', 'scroll')
+            .scrollLeft(xf, dt, t => t*(2-t));
+        }, 0);
       }
     }
   };
